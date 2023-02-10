9820a3e4171866fb5c4426f8cce9d0380bce8d06
Mauricio Santos <mauwebjc@gmail.com>
2023-02-09 22:06:49 -0300

    [Modulo01] Inclusão de regras TemplateRef

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  ModalService,
+  ModalRef,
+} from "./shared/components/modal/services/modal.service";
+import { Component, ViewChild, TemplateRef } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'angular-avancando-no-design-de-componentes-com-acessibilidade';
+  @ViewChild("modal") public modalTemplateRef!: TemplateRef<any>;
+  title = "angular-avancando-no-design-de-componentes-com-acessibilidade";
+  public firstName = "Mauricio";
+  public modalRef!: ModalRef;
+
+  constructor(private modalService: ModalService) {}
+
+  public show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: "Detalhes do usuário",
+    });
+  }
 }
